1ee486c77d7adf0a8c5cfa596b448c2a1f669adb
PanWilson <PanWilsonn@wp.pl>
2018-02-26 00:14:57 +0100

    first works on movement

---
+++ Cebularz/Assets/Scripts/TouchManagment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManagment : MonoBehaviour {
+
+
+
+    public LayerMask mask;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.touchCount >= 2)
+        {
+            RaycastHit2D[] hits;
+            ContactFilter2D Filter = new ContactFilter2D();
+            
+            hits = Physics2D.BoxCastAll(
+                Input.GetTouch(0).position,
+                new Vector2(Mathf.Abs(Input.GetTouch(1).position.x-Input.GetTouch(0).position.x)/2, Mathf.Abs(Input.GetTouch(1).position.y - Input.GetTouch(0).position.y)/2),
+                0,
+                new Vector2(0,0),
+                Filter,
+                hits
+                );
+        }
+
+	}
+
+}
---
+++ Cebularz/Assets/Scripts/Walking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Walking : MonoBehaviour {
+
+
+    NavMeshAgent NavAgent;
+
+
+    // Use this for initialization
+    void Start () {
+
+        NavAgent = GetComponent<NavMeshAgent>();
+        NavAgent.updateUpAxis = false;
+        NavAgent.updateRotation = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void MoveTo(Vector3 location)
+    {
+        NavAgent.destination = location;
+    }
+}
